15aa7a84601379945fc0e2c0f94c899821af9b7b
JFY96 <36199863+JFY96@users.noreply.github.com>
2021-08-26 23:38:26 +0100

    Converted App class component to functional component. Moved todo list logic to Overview component using hooks and now store todo list in localStorage

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import uniqid from 'uniqid';
 
-import TaskCreate from './TaskCreate';
 import Task from './Task';
-import './Overview.scss';
+import TaskCreate from './TaskCreate';
 import TaskEdit from './TaskEdit';
+import './Overview.scss';
 
-const Overview = ({ 
-	taskToCreate,
-	tasks,
-	editTaskId,
-	handleCreateTaskChange,
-	addTask,
-	completeTask,
-	deleteTask,
-	editTask,
-	editSave,
-	editCancel,
-	editInputRef,
-}) => {
+const getInitialTaskState = () => ({
+	id: uniqid(),
+	text: '',
+	complete: false,
+});
+
+const Overview = () => {
+	const [taskToCreate, setTaskToCreate] = useState(getInitialTaskState());
+	const [tasks, setTasks] = useState(localStorage.getItem('tasks') ?  JSON.parse(localStorage.getItem('tasks')) : []);
+	const [editTaskId, setEditTaskId] = useState('');
+
+	const editInputRef = useRef(null);
+
+	useEffect(() => {
+		if (editTaskId !== '') editInputRef.current.focus();
+	}, [editTaskId]);
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
+
+	const handleCreateTaskChange = (propertyName, val) => {
+		setTaskToCreate(prevTaskToCreate => ({
+			...prevTaskToCreate,
+			[propertyName]: val,
+		}))
+	};
+
+	const addTask = () => {
+		if (taskToCreate.text.trim() === '') return;
+		setTasks(prevTasks => prevTasks.concat(taskToCreate));
+		setTaskToCreate(getInitialTaskState());
+	};
+
+	const completeTask = (id) => {
+		setTasks(prevTasks => prevTasks.map(task => task.id === id ? { ...task, complete: !task.complete } : task));
+	};
+
+	const deleteTask = (id) => {
+		setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+	};
+
+	const editTask = (id) => setEditTaskId(id);
+
+	const editSave = (text) => {
+		setTasks(prevTasks => prevTasks.map(task => task.id === editTaskId ? { ...task, text } : task));
+		setEditTaskId('');
+	};
+
+	const editCancel = () => setEditTaskId('');
+
 	return (
 		<div className="overview">
 			<TaskCreate 
