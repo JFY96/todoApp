a0345fe8e26117e549e5e1bce6ca925551eccf6b
JFY96 <36199863+JFY96@users.noreply.github.com>
2021-08-26 23:26:11 +0100

    Configure web-dev-server and remove react-scripts created from create-react-app

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		modules: [path.join(__dirname, 'src'), 'node_modules'],
+		alias: {
+		  react: path.join(__dirname, 'node_modules', 'react'),
+		},
 	},
 	module: {
 		rules: [
@@ -31,4 +38,9 @@ module.exports = {
 			},
 		]
 	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		  template: './index.html',
+		}),
+	]
 };
